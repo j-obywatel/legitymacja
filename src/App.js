4adc0f74b34a6d75c1efa5e7861bab12682c9e7f
Kuba Karnath <jacob_jk@wp.pl>
2024-08-20 17:17:35 +0200

    newcom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
+  const myDate = date.toLocaleString("pl-PL", {
+    minute: "numeric",
+    hour: "2-digit",
+    second: "2-digit",
+  });
+  const mySecondDate = date.toLocaleString("pl-PL", {
+    day: "numeric",
+    month: "numeric",
+    year: "numeric",
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calosc">
+      <div className="gora"></div>
+      <div className="date">
+        Czas: {myDate} {""}
+        {mySecondDate}
+      </div>
+      <div className="cialo">
+        <img
+          src="https://jacob-jk.github.io/legitymacja/images/zdjecie.png"
+          alt="profil"
+          className="profil"
+        />
+        <img
+          src="https://jacob-jk.github.io/legitymacja/images/flaga.gif"
+          alt="flaga"
+          className="flaga"
+        />
+      </div>
+      <div className="dol"></div>
     </div>
   );
 }
